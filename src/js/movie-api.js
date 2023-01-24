c5157b05ad712a0ca18e0e9677d2f0ad9aa92a13
Oleksandra Tiankina <ag7756862@gmail.com>
2023-01-24 17:47:52 +0200

    init: start header markup

---
+++ src/js/movie-api.js
@@ -0,0 +1,150 @@
+'use strict';
+
+import axios from 'axios';
+
+export default class TheMovieAPI {
+  static BASE_URL = 'https://api.themoviedb.org/3/';
+  static API_KEY = 'cf0ab519f45eea0dacef149b4aa4a796';
+  static TRENDING_URL = 'trending/movie/week';
+  static SEARCH = `search/movie`;
+  static GENRES_LIST = `genre/movie/list`;
+
+  constructor() {
+    this.page = 1;
+    this.perPage = 100;
+    this.searchQuery = '';
+    this.movieId = null;
+    this.genresId = null;
+  }
+
+  // Get trending movies per last Week:
+  async fetchTrendingFilms() {
+    const searchParams = {
+      params: {
+        api_key: TheMovieAPI.API_KEY,
+        page: this.page,
+      },
+    };
+
+    try {
+      const response = await axios.get(
+        `${TheMovieAPI.BASE_URL}${TheMovieAPI.TRENDING_URL}`,
+        searchParams
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // Search for movies:
+  async fetchSearchFilms() {
+    try {
+      const searchParams = {
+        params: {
+          api_key: TheMovieAPI.API_KEY,
+          page: this.page,
+          include_adult: false,
+          query: this.searchQuery,
+          language: 'en - US'
+        },
+      };
+      const response = await axios.get(
+        `${TheMovieAPI.BASE_URL}${TheMovieAPI.SEARCH}`,
+        searchParams
+      );
+      this.incrementPage();
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Search for genres:
+  async getGenres() {
+    try {
+      const searchParams = {
+        params: {
+          api_key: TheMovieAPI.API_KEY,
+        },
+      };
+      const response = await axios.get(
+        `${TheMovieAPI.BASE_URL}${TheMovieAPI.GENRES_LIST}`,
+        searchParams
+      );
+      return response.data.genres; // Повертає проміс із жанрами
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Get the primary information about a movie:
+
+  async getInfoMovie() {
+    try {
+      const searchParams = {
+        params: {
+          api_key: TheMovieAPI.API_KEY,
+          language: 'en - US',
+        },
+      };
+      const response = await axios.get(
+        `${TheMovieAPI.BASE_URL}movie/${this.movieId}`,
+        searchParams
+      );
+
+      this.incrementPage();
+      response.data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Get the videos that have been added to a movie:
+
+  async getTrailerFilm(id) {
+    console.log(id);
+    let trailerKey = '';
+    try {
+      const searchParams = {
+        params: {
+          api_key: TheMovieAPI.API_KEY,
+          language: 'en - US',
+        },
+      };
+      const response = await axios.get(
+        `${TheMovieAPI.BASE_URL}movie/${id}/videos`,
+        searchParams
+      );
+
+      const data = response.data.results;
+
+      data.forEach(data => {
+        if (data.type === 'Trailer') {
+          trailerKey = {
+            key: data.key,
+          };
+        }
+      });
+
+      localStorage.setItem(TRAILER_KEY, JSON.stringify(trailerKey));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/render_trending.js
@@ -0,0 +1,72 @@
+import { createCardMarkup } from './card_markup';
+import { QueryHandler } from './query_handler';
+import { Pagination } from './pagination';
+import methodsStorage from './locale-storage-methods';
+import { FILMS, LAST_PAGE } from './localStorageKeys';
+import { refs } from './refs';
+
+const queryHandler = new QueryHandler();
+export const pagination = new Pagination();
+
+export function renderMarkupTrending() {
+  let page = 1;
+  const lastPage = methodsStorage.load(LAST_PAGE);
+  if (lastPage) {
+    page = lastPage;
+  }
+
+  return queryHandler
+    .fetchQueryResultsForTrending(page)
+    .then(data => {
+      const { page, total_pages, results } = data;
+      methodsStorage.save(FILMS, results);
+      const markup = results.map(createCardMarkup).join('');
+      refs.filmListRef.innerHTML = markup;
+
+      if (total_pages > 1) {
+        pagination.totalPages = total_pages;
+        pagination.page = page;
+        pagination.fetch = page =>
+          queryHandler.fetchQueryResultsForTrending(page);
+        pagination.renderMarkup();
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+refs.pagination.addEventListener('click', onChangePageClick);
+
+export function onChangePageClick(e) {
+  if (e.target.nodeName === 'UL') {
+    return;
+  }
+  if (e.target.className === 'btn__next') {
+    pagination.incrementPage();
+  }
+  if (e.target.className === 'btn__prev') {
+    pagination.decrementPage();
+  }
+  if (e.target.className === 'dots') {
+    return;
+  }
+  if (e.target.className === 'num') {
+    pagination.page = Number(e.target.textContent);
+  }
+  
+  
+  if (pagination.fetch.toString().includes('Trending')) {
+    methodsStorage.save(LAST_PAGE, pagination.page);
+  }
+  
+  pagination
+    .fetch(pagination.page)
+    .then(({ results }) => {
+      console.log(pagination.fetch);
+      const markup = results.map(createCardMarkup).join('');
+      methodsStorage.save(FILMS, results);
+      refs.filmListRef.innerHTML = markup;
+    })
+    .catch(error => console.log(error));
+
+  pagination.renderMarkup();
+}
